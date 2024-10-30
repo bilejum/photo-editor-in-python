37a2c33f14c930ead4dd31c3acc294a3bf97e723
bilejum <1776259607@163.com>
2024-10-31 02:53:05 +0800

    print photo info

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from PIL import Image
 
 im = Image.open("img\泉此方 壁纸.jpg")
-im.show()
+
+# im = im.rotate(45)
+# im.show()
+print(im.filename)
+print(im.size)
+print(im.width)
+print(im.height)
+print(im.info)
